4d994db7aa1e6adf6569e19cae413169d4178e32
Derek James <derekjames1093@gmail.com>
2020-11-20 11:52:49 -0600

    update

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter)
 //TODO DOUBLE CHECK
 app.use('/api/hub', hubRouter )
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
